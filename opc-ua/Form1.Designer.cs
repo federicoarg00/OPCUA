e58a42cbef7ce993d33d51d3f4d19db9eea5da9a
mohsen_mokhtari <wav1376@gmail.com>
2019-03-03 05:30:29 +0330

    git init

---
+++ opc-ua/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace opc_ua
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectButton = new System.Windows.Forms.Button();
+            this.readButton = new System.Windows.Forms.Button();
+            this.serverAddress = new System.Windows.Forms.TextBox();
+            this.nodeToWrite = new System.Windows.Forms.TextBox();
+            this.valueToWrite = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.connectStatus = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.valueOfRead = new System.Windows.Forms.Label();
+            this.valueAfterWrite = new System.Windows.Forms.Label();
+            this.nodeToRead = new System.Windows.Forms.TextBox();
+            this.writeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(556, 61);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 0;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(556, 172);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(75, 23);
+            this.readButton.TabIndex = 0;
+            this.readButton.Text = "Read";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.readButton_Click);
+            // 
+            // serverAddress
+            // 
+            this.serverAddress.Location = new System.Drawing.Point(229, 61);
+            this.serverAddress.Name = "serverAddress";
+            this.serverAddress.Size = new System.Drawing.Size(282, 22);
+            this.serverAddress.TabIndex = 1;
+            // 
+            // nodeToWrite
+            // 
+            this.nodeToWrite.Location = new System.Drawing.Point(229, 290);
+            this.nodeToWrite.Name = "nodeToWrite";
+            this.nodeToWrite.Size = new System.Drawing.Size(282, 22);
+            this.nodeToWrite.TabIndex = 1;
+            // 
+            // valueToWrite
+            // 
+            this.valueToWrite.Location = new System.Drawing.Point(229, 341);
+            this.valueToWrite.Name = "valueToWrite";
+            this.valueToWrite.Size = new System.Drawing.Size(282, 22);
+            this.valueToWrite.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server Address : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Conection Status";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(89, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(89, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Node Value";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(89, 297);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Node To Write ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(89, 346);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 17);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Value To Write";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(89, 384);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 17);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "New Value";
+            // 
+            // connectStatus
+            // 
+            this.connectStatus.AutoSize = true;
+            this.connectStatus.Location = new System.Drawing.Point(226, 102);
+            this.connectStatus.Name = "connectStatus";
+            this.connectStatus.Size = new System.Drawing.Size(32, 17);
+            this.connectStatus.TabIndex = 2;
+            this.connectStatus.Text = "???";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(89, 172);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(101, 17);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "Node To Read";
+            // 
+            // valueOfRead
+            // 
+            this.valueOfRead.AutoSize = true;
+            this.valueOfRead.Location = new System.Drawing.Point(226, 220);
+            this.valueOfRead.Name = "valueOfRead";
+            this.valueOfRead.Size = new System.Drawing.Size(32, 17);
+            this.valueOfRead.TabIndex = 2;
+            this.valueOfRead.Text = "???";
+            // 
+            // valueAfterWrite
+            // 
+            this.valueAfterWrite.AutoSize = true;
+            this.valueAfterWrite.Location = new System.Drawing.Point(226, 384);
+            this.valueAfterWrite.Name = "valueAfterWrite";
+            this.valueAfterWrite.Size = new System.Drawing.Size(32, 17);
+            this.valueAfterWrite.TabIndex = 2;
+            this.valueAfterWrite.Text = "???";
+            // 
+            // nodeToRead
+            // 
+            this.nodeToRead.Location = new System.Drawing.Point(229, 169);
+            this.nodeToRead.Name = "nodeToRead";
+            this.nodeToRead.Size = new System.Drawing.Size(282, 22);
+            this.nodeToRead.TabIndex = 1;
+            this.nodeToRead.TextChanged += new System.EventHandler(this.nodeToRead_TextChanged);
+            // 
+            // writeButton
+            // 
+            this.writeButton.Location = new System.Drawing.Point(556, 290);
+            this.writeButton.Name = "writeButton";
+            this.writeButton.Size = new System.Drawing.Size(75, 23);
+            this.writeButton.TabIndex = 3;
+            this.writeButton.Text = "Write";
+            this.writeButton.UseVisualStyleBackColor = true;
+            this.writeButton.Click += new System.EventHandler(this.writeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.writeButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.valueAfterWrite);
+            this.Controls.Add(this.valueOfRead);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.connectStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.valueToWrite);
+            this.Controls.Add(this.nodeToWrite);
+            this.Controls.Add(this.nodeToRead);
+            this.Controls.Add(this.serverAddress);
+            this.Controls.Add(this.readButton);
+            this.Controls.Add(this.connectButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.TextBox serverAddress;
+        private System.Windows.Forms.TextBox nodeToWrite;
+        private System.Windows.Forms.TextBox valueToWrite;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label connectStatus;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label valueOfRead;
+        private System.Windows.Forms.Label valueAfterWrite;
+        private System.Windows.Forms.TextBox nodeToRead;
+        private System.Windows.Forms.Button writeButton;
+    }
+}
+
---
+++ opc-ua/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hylasoft.Opc.Ua;
+using System.Net.NetworkInformation;
+
+namespace opc_ua
+{
+    public partial class Form1 : Form
+    {
+        private UaClient clientTCP;
+        public static string localIP = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            connectStatus.Text = "Not connected";
+            string localIP = "";
+            nodeToRead.Text = "Simulation.Random1";
+            nodeToWrite.Text = "Simulation.Random1";
+
+            // GET IP Address
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 || ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                {
+
+                    if (ni.Name == "Wi-Fi")
+                    {
+                        foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+                        {
+                            if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                            {
+                                localIP = ip.Address.ToString();
+                                serverAddress.Text = "opc.tcp://" + ip.Address.ToString() + ":53530/OPCUA/SimulationServer";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            clientTCP = new UaClient(new Uri(serverAddress.Text));
+            clientTCP.Connect();
+            connectStatus.Text = "Connected";
+        }
+
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            string node = nodeToRead.Text;
+            string nodeType = clientTCP.GetDataType(node).ToString();
+            string myValue = "";
+            switch (nodeType)
+            {
+                case "System.Double":
+                    myValue = clientTCP.Read<double>(node).Value.ToString();
+                    break;
+                case "System.Float":
+                    myValue = clientTCP.Read<float>(node).Value.ToString();
+                    break;
+                case "System.Int32":
+                    myValue = clientTCP.Read<Int32>(node).Value.ToString();
+                    break;
+            }
+
+            valueOfRead.Text = myValue.ToString();
+        }
+
+
+
+        private void writeButton_Click(object sender, EventArgs e)
+        {
+            int flag=0;
+            var node = nodeToWrite.Text;
+            string nodeType = clientTCP.GetDataType(node).ToString();
+            float floatMyValue=0;double doubleMyValue=0;Int32 int32MyValue=0;
+            switch (nodeType)
+            {
+                case "System.Double":
+                    doubleMyValue = Convert.ToDouble(valueToWrite.Text);
+                    flag = 1;
+                    break;
+                case "System.Float":
+                    floatMyValue = (float)Convert.ToDouble(valueToWrite.Text);
+                    flag = 2;
+                    break;
+                case "System.Int32":
+                    int32MyValue = Convert.ToInt32(valueToWrite.Text);
+                    flag = 3;
+                    break;
+            }
+            if (flag == 1) { clientTCP.Write(node.ToString(), doubleMyValue); valueAfterWrite.Text = doubleMyValue.ToString(); }
+            else if (flag == 2) { clientTCP.Write(node.ToString(), floatMyValue); valueAfterWrite.Text = floatMyValue.ToString(); }
+            else if (flag == 3) { clientTCP.Write(node.ToString(), int32MyValue); valueAfterWrite.Text = int32MyValue.ToString(); }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nodeToRead_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
